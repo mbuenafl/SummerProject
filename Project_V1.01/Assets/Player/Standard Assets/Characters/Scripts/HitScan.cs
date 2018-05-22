f57aaa5bd4baee88da3836222777ff1d48739816
Mark Buenaflor <mbuenafl@asu.edu>
2018-05-22 15:00:47 -0700

    Sword + Raycast
    * There's a sword in there with a bug that makes it appear in the center of the player and resize
    
    *There's now a raycast system

---
+++ Project_V1.01/Assets/Player/Standard Assets/Characters/Scripts/HitScan.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitScan : MonoBehaviour {
+    //Melee attack range
+    public float meleeRange = 10f;
+    public GameObject sword;
+
+    //Get the camera that is going to be used 
+    private Camera fpsCam;
+
+	// Use this for initialization
+	void Start () {
+        fpsCam = GetComponentInChildren<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+
+        //Any input that is a fire trigger
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 screenMiddle = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f));
+            RaycastHit meleeHit;
+
+            if(Physics.Raycast(screenMiddle, fpsCam.transform.forward, out meleeHit, meleeRange))
+            {
+                if(meleeHit.collider.tag == "Enemy")
+                {
+                    //Do what ever needs to happen to the enemy
+
+                    Debug.Log("Hit an Enemy");
+                }
+            }
+        }
+	}
+}
---
+++ Project_V1.01/Assets/Weapons/Sword/swordScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swordScript : MonoBehaviour
+{
+
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")){
+            anim.SetBool("swing", true);
+        }
+        else{
+            anim.SetBool("swing", false);
+        }
+    }
+}
