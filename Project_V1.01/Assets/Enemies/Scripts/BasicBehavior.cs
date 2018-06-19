09bf2ecf268637287bb60863f0b982599873141c
Mark Buenaflor <mbuenafl@asu.edu>
2018-06-18 20:34:51 -0700

    AI start
    Started making the AI, having problems with the Nav. The aggro range is in place and is usable.

---
+++ Project_V1.01/Assets/Enemies/Scripts/BasicBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicBehavior : MonoBehaviour {
+
+    Transform target; // use this variable as the player model
+    NavMeshAgent agent;
+    public float aggro_range = 10f;
+
+
+
+	// Use this for initialization
+	void Start () {
+        target = playerManager.instance.Player.transform;
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if(distance <= aggro_range)
+        {
+            agent.SetDestination(target.position);
+        }
+	}
+
+ 
+    // This is where the range is made, it will be a red circle.
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, aggro_range);
+    }
+}
---
+++ Project_V1.01/Assets/Enemies/Scripts/playerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerManager : MonoBehaviour {
+
+    #region Singleton
+
+    public static playerManager instance;
+    
+    void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion // Player singleton
+
+    public GameObject Player;
+
+}
